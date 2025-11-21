26b37869a9dacdf9ba96bf04a8027390982f9d68
Affan Suhendar <affansuhendar1@gmail.com>
2025-11-22 01:26:08 +0700

    first commit

--- Scaler.py
+++ Scaler.py
@@ -23,7 +23,10 @@ def Normalize(df, numerical_cols):
 
             st.session_state["df_Normalize"] = df.copy()
 
-        st.write(st.session_state.get("df_Normalize"))
+        normalized = st.session_state.get("df_Normalize")
+
+        st.dataframe(normalized.head(10), use_container_width=True)
+        st.download_button("Download Normalized Data", normalized.to_csv().encode('utf-8'), "Normalized_data.csv", "text/csv", key='download-csv')
         
 
     
